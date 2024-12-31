3e1bbd8e2e6dc07e89cdaea8c0577a38043fad23
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-31 14:39:29 +0800

    Create time state to store the current time

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Clock() {
-	// Declare state to get the current time
+	// Declare state to store the current time
+	const [time, setTime] = useState(new Date());
+
 	// Use useEffect to start the interval (every one second)
 	// Format the time
 	// Add zero to the front if the number is less than 10
