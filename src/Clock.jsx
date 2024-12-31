3572385a52b0a6a42b6fbcd492e93f6b89f0c249
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-31 14:43:12 +0800

    Start interval to run every second when component first mount
    - Interval will set the state to the current time

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -1,10 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Clock() {
 	// Declare state to store the current time
 	const [time, setTime] = useState(new Date());
 
 	// Use useEffect to start the interval (every one second)
+	useEffect(() => {
+		const intervalId = setInterval(() => {
+			setTime(new Date());
+		}, 1000);
+	}, []);
+
 	// Format the time
 	// Add zero to the front if the number is less than 10
 
